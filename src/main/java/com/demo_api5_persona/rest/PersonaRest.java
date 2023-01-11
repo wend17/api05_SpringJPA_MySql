9ad81e50a2076db4a4f4334974fbadb9509b692d
Wendy <wendyvillazana@gmail.com>
2023-01-11 17:03:15 -0500

    añadiendo script en static

--- src/main/java/com/demo_api5_persona/rest/PersonaRest.java
+++ src/main/java/com/demo_api5_persona/rest/PersonaRest.java
@@ -5,7 +5,7 @@ import com.demo_api5_persona.service.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
